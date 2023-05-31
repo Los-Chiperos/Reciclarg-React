1dcdd27cbfada4a8281ad18e71ecf3a84ce9b3be
Nicolas <nicolaszelarayan.ez@gmail.com>
2023-05-31 10:55:34 -0300

    se agrega seccion FAQ

--- src/components/pages/Faq.jsx
+++ src/components/pages/Faq.jsx
@@ -36,7 +36,7 @@ const Faq = () => {
   ];
 
   return (
-    <div className="w-full bg-gray-50 bg-cover" style={{ backgroundImage: `url(${FaqImage})`}}>
+    <div class="w-full bg-gray-50 bg-cover" style="background-image: url('https://www.ecovidrio.es/sites/default/files/2019-10/background-home-1.png')">
       {/* :TITLE & IMAGE */}
       <div className="my-8 mx-auto max-w-4xl flex flex-col items-center">
         <img src="/img/ecovidrio-bg-inv.jpg" alt="Frequently Asked Questions" className="w-full h-full" />
