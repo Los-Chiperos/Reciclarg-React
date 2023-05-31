6fee2859357eb9005feb5ab64011a5268c89008c
Nicolas <nicolaszelarayan.ez@gmail.com>
2023-05-30 22:23:17 -0300

    fix navbar section Nosotros

--- src/components/pages/Nosotros.jsx
+++ src/components/pages/Nosotros.jsx
@@ -1,39 +1,54 @@
-import Footer from "../layout/Footer"
-import Heder from "../layout/Heder"
+import Footer from "../layout/Footer";
+import Header from "../layout/Header";
+
 function Nosotros() {
   return (
-        <>
-        <Heder/>
-      <div>
-      <div
-        className="container mx-auto py-8 h-screen background-image bg-cover"
-        style={{
-          backgroundImage:
-            "url('https://www.ecovidrio.es/sites/default/files/2019-10/background-home-1.png')",
-        }}
-      >
-        <div className="flex flex-col text-center gap-4 mb-8 p-4 ">
-          <div className="text-green-600  text-3xl md:text-4xl font-bold">
-            Somos alumnos de la cohorte 2022 de la tecnicatura en programcion
+    <>
+      <Header />
+
+      <div className="container mx-auto py-8 h-screen background-image bg-cover">
+        <div className="flex flex-col text-center gap-4 mb-8 p-4">
+          <div className="text-green-600 text-3xl md:text-4xl font-bold">
+            Somos alumnos de la cohorte 2022 de la tecnicatura en programación
           </div>
           <div className="text-gray-600 md:text-lg">
-            de distintas partes del pais unidos por la pasion hacia la
-            tecnologia
+            De distintas partes del país unidos por la pasión hacia la tecnología
           </div>
         </div>
 
         <div className="flex items-center justify-center flex-wrap gap-4 p-4">
+          {/* Tarjeta 1 */}
           <div className="card bg-amber-100">
             <img
-              src="./pofile.jpeg"
+              src="./pofile1.jpeg"
               className="w-24 h-24 rounded-full object-cover transition duration-200 hover:scale-110"
             />
             <div className="text-gray-900 text-lg font-bold">
-              Nicolas Zelarayan
+              Miembro 1
             </div>
-            <div className="text-green-600 font-semibold">DevOps Engineer</div>
+            <div className="text-green-600 font-semibold">Ocupación 1</div>
             <div className="text-gray-600" style={{ textAlign: "center" }}>
-              Cloud Architect Designing Scalable and Resilient Solutions.
+              Descripción del miembro 1.
+            </div>
+            <div className="flex items-center justify-center gap-3 mt-2 w-auto h-5 text-gray-600">
+              <i className="fa-brands fa-twitter fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-linkedin fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-github fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+            </div>
+          </div>
+            
+                    {/* Tarjeta 1 */}
+                    <div className="card bg-amber-100">
+            <img
+              src="./pofile1.jpeg"
+              className="w-24 h-24 rounded-full object-cover transition duration-200 hover:scale-110"
+            />
+            <div className="text-gray-900 text-lg font-bold">
+              Miembro 1
+            </div>
+            <div className="text-green-600 font-semibold">Ocupación 1</div>
+            <div className="text-gray-600" style={{ textAlign: "center" }}>
+              Descripción del miembro 1.
             </div>
             <div className="flex items-center justify-center gap-3 mt-2 w-auto h-5 text-gray-600">
               <i className="fa-brands fa-twitter fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
@@ -42,12 +57,131 @@ function Nosotros() {
             </div>
           </div>
 
-          {/* Repite el código anterior para cada miembro del equipo */}
+                    {/* Tarjeta 1 */}
+                    <div className="card bg-amber-100">
+            <img
+              src="./pofile1.jpeg"
+              className="w-24 h-24 rounded-full object-cover transition duration-200 hover:scale-110"
+            />
+            <div className="text-gray-900 text-lg font-bold">
+              Miembro 1
+            </div>
+            <div className="text-green-600 font-semibold">Ocupación 1</div>
+            <div className="text-gray-600" style={{ textAlign: "center" }}>
+              Descripción del miembro 1.
+            </div>
+            <div className="flex items-center justify-center gap-3 mt-2 w-auto h-5 text-gray-600">
+              <i className="fa-brands fa-twitter fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-linkedin fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-github fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+            </div>
+          </div>
+
+                    {/* Tarjeta 1 */}
+                    <div className="card bg-amber-100">
+            <img
+              src="./pofile1.jpeg"
+              className="w-24 h-24 rounded-full object-cover transition duration-200 hover:scale-110"
+            />
+            <div className="text-gray-900 text-lg font-bold">
+              Miembro 1
+            </div>
+            <div className="text-green-600 font-semibold">Ocupación 1</div>
+            <div className="text-gray-600" style={{ textAlign: "center" }}>
+              Descripción del miembro 1.
+            </div>
+            <div className="flex items-center justify-center gap-3 mt-2 w-auto h-5 text-gray-600">
+              <i className="fa-brands fa-twitter fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-linkedin fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-github fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+            </div>
+          </div>
+
+                    {/* Tarjeta 1 */}
+                    <div className="card bg-amber-100">
+            <img
+              src="./pofile1.jpeg"
+              className="w-24 h-24 rounded-full object-cover transition duration-200 hover:scale-110"
+            />
+            <div className="text-gray-900 text-lg font-bold">
+              Miembro 1
+            </div>
+            <div className="text-green-600 font-semibold">Ocupación 1</div>
+            <div className="text-gray-600" style={{ textAlign: "center" }}>
+              Descripción del miembro 1.
+            </div>
+            <div className="flex items-center justify-center gap-3 mt-2 w-auto h-5 text-gray-600">
+              <i className="fa-brands fa-twitter fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-linkedin fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-github fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+            </div>
+          </div>
+
+                    {/* Tarjeta 1 */}
+                    <div className="card bg-amber-100">
+            <img
+              src="./pofile1.jpeg"
+              className="w-24 h-24 rounded-full object-cover transition duration-200 hover:scale-110"
+            />
+            <div className="text-gray-900 text-lg font-bold">
+              Miembro 1
+            </div>
+            <div className="text-green-600 font-semibold">Ocupación 1</div>
+            <div className="text-gray-600" style={{ textAlign: "center" }}>
+              Descripción del miembro 1.
+            </div>
+            <div className="flex items-center justify-center gap-3 mt-2 w-auto h-5 text-gray-600">
+              <i className="fa-brands fa-twitter fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-linkedin fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-github fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+            </div>
+          </div>
+
+                    {/* Tarjeta 1 */}
+                    <div className="card bg-amber-100">
+            <img
+              src="./pofile1.jpeg"
+              className="w-24 h-24 rounded-full object-cover transition duration-200 hover:scale-110"
+            />
+            <div className="text-gray-900 text-lg font-bold">
+              Miembro 1
+            </div>
+            <div className="text-green-600 font-semibold">Ocupación 1</div>
+            <div className="text-gray-600" style={{ textAlign: "center" }}>
+              Descripción del miembro 1.
+            </div>
+            <div className="flex items-center justify-center gap-3 mt-2 w-auto h-5 text-gray-600">
+              <i className="fa-brands fa-twitter fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-linkedin fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-github fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+            </div>
+          </div>
+
+                    {/* Tarjeta 1 */}
+                    <div className="card bg-amber-100">
+            <img
+              src="./pofile1.jpeg"
+              className="w-24 h-24 rounded-full object-cover transition duration-200 hover:scale-110"
+            />
+            <div className="text-gray-900 text-lg font-bold">
+              Miembro 1
+            </div>
+            <div className="text-green-600 font-semibold">Ocupación 1</div>
+            <div className="text-gray-600" style={{ textAlign: "center" }}>
+              Descripción del miembro 1.
+            </div>
+            <div className="flex items-center justify-center gap-3 mt-2 w-auto h-5 text-gray-600">
+              <i className="fa-brands fa-twitter fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-linkedin fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+              <i className="fa-brands fa-github fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <Footer/>
-        </>
+
+      <Footer />
+    </>
   );
 }
+
 export default Nosotros;
