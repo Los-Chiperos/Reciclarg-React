d35758f0e39ab6e4b71669aec593c396868112d6
Nicolas <nicolaszelarayan.ez@gmail.com>
2023-06-06 00:31:50 -0300

    finalizamos edicion page Nosotros

--- src/components/pages/Nosotros.jsx
+++ src/components/pages/Nosotros.jsx
@@ -1,54 +1,87 @@
 import React from "react";
-import profileImg from "../img/profile.png";
+import Nicolas from "../img/nicolas.jpeg";
+import David from "../img/david.jpeg";
+import Cristian from "../img/cristian.png";
+import Gonzalo from "../img/gonzalo.jpeg";
+import Juan from "../img/juan.jpeg";
+import Agustin from "../img/agustin.jpeg";
+import Angelo from "../img/angelo.jpeg";
+import Leonardo from "../img/leonardo.jpeg";
+
+
 
 const teamMembers = [
   {
-    name: "Nicolas Zelarayan",
-    role: "DevOps Engineer",
-    description: "Cloud Architect Designing Scalable and Resilient Solutions.",
-    imgSrc: profileImg,
+    name: "Cristian Alderete",
+    role: "Tecnico en Programcion",
+    description: "Backend Developer Api Rest  MySQL JAVA",
+    imgSrc: Cristian,
+    twitterUrl: "https://twitter.com/cristian",
+    linkedinUrl: "https://www.linkedin.com/in/calderete/",
+    githubUrl: "https://github.com/catastones",
+  },
+  {
+    name: "Gonzalo Araya",
+    role: "Tecnico en Programcion",
+    description: "Fullstack Developer React Nodejs MongoDB",
+    imgSrc: Gonzalo,
+    twitterUrl: "https://twitter.com/cristian",
+    linkedinUrl: "https://www.linkedin.com/in/gonzalo-araya-416a6520b/",
+    githubUrl: "https://github.com/gonzalo991",
+  },
+  {
+    name: "Juan Schallmoser",
+    role: "Tecnico en Programcion",
+    description: "FullStack Developer UX-UI React HTML CSS",
+    imgSrc: Juan,
+    twitterUrl: "https://twitter.com/SchallmoserJuan",
+    linkedinUrl: "https://www.linkedin.com/in/juanschallmoser/",
+    githubUrl: "https://github.com/SchallmoserJuan",
+  },
+  {
+    name: "Agustin Gomez Aranda",
+    role: "Tecnico en Programcion",
+    description: "Frontend Developer React Html CSS JavaScript",
+    imgSrc: Agustin,
+    twitterUrl: "https://twitter.com/cristian",
+    linkedinUrl: "https://www.linkedin.com/in/agustin-gomez-aranda/",
+    githubUrl: "https://github.com/Chelito2014",
   },
   {
     name: "Nicolas Zelarayan",
-    role: "DevOps Engineer",
-    description: "Cloud Architect Designing Scalable and Resilient Solutions.",
-    imgSrc: profileImg,
+    role: "Tecnico en Programcion",
+    description: "DevOps especializado en infraestructura cloud",
+    imgSrc: Nicolas,
+    twitterUrl: "https://twitter.com/zelarayanFN?t=oaYtXi2vCvVboZCf51hahQ&s=09",
+    linkedinUrl: "https://www.linkedin.com/in/fernando-nicolas-zelarayan/",
+    githubUrl: "https://github.com/NicolasZelarayan",
   },
   {
-    name: "Nicolas Zelarayan",
-    role: "DevOps Engineer",
-    description: "Cloud Architect Designing Scalable and Resilient Solutions.",
-    imgSrc: profileImg,
+    name: "David Mercado",
+    role: "Tecnico en Programcion",
+    description: "Frontend Developer React Html JavaScript",
+    imgSrc: David,
+    twitterUrl: "https://twitter.com/DeividMercad2?t=D3KzeA3BNF1L29VZQFY8Kg&s=08",
+    linkedinUrl: "https://www.linkedin.com/in/david-mercado-510721176/",
+    githubUrl: "https://github.com/dav1dM3rcado",
   },
   {
-    name: "Nicolas Zelarayan",
-    role: "DevOps Engineer",
-    description: "Cloud Architect Designing Scalable and Resilient Solutions.",
-    imgSrc: profileImg,
+    name: "Angelo Pertus",
+    role: "Tecnico en Programcion",
+    description: "WEB Developer React Nodejs MongoDB",
+    imgSrc: Angelo,
+    twitterUrl: "https://twitter.com/",
+    linkedinUrl: "https://www.linkedin.com/in/angelo-pertus-234795201/",
+    githubUrl: "https://github.com/Angelopertus",
   },
   {
-    name: "Nicolas Zelarayan",
-    role: "DevOps Engineer",
-    description: "Cloud Architect Designing Scalable and Resilient Solutions.",
-    imgSrc: profileImg,
-  },
-  {
-    name: "Nicolas Zelarayan",
-    role: "DevOps Engineer",
-    description: "Cloud Architect Designing Scalable and Resilient Solutions.",
-    imgSrc: profileImg,
-  },
-  {
-    name: "Nicolas Zelarayan",
-    role: "DevOps Engineer",
-    description: "Cloud Architect Designing Scalable and Resilient Solutions.",
-    imgSrc: profileImg,
-  },
-  {
-    name: "Nicolas Zelarayan",
-    role: "DevOps Engineer",
-    description: "Cloud Architect Designing Scalable and Resilient Solutions.",
-    imgSrc: profileImg,
+    name: "Leonardo Valdez",
+    role: "Tecnico en Programcion",
+    description: "SysAdmin especializado en desarrollo web",
+    imgSrc: Leonardo,
+    twitterUrl: "https://twitter.com/",
+    linkedinUrl: "https://www.linkedin.com/in/leonardo-javier-valdez-19106326a/",
+    githubUrl: "https://github.com/Leonardo-Valdez",
   },
   // Agrega el resto de los miembros del equipo aquí
   // ...
@@ -56,10 +89,10 @@ const teamMembers = [
 
 function ProfileCard({ member }) {
   return (
-    <div className="card bg-amber-100 rounded-lg p-4 text-center w-64 h-85">  
+    <div className="card bg-amber-100 rounded-lg p-4 text-center w-64 h-85 mx-4">  
       <img
         src={member.imgSrc}
-        className="w-24 h-24 rounded-full transition duration-200 hover:scale-110 mx-auto" // Eliminamos object-cover, añadimos centrado horizontal
+        className="w-24 h-24 rounded-full transition duration-200 hover:scale-110 mx-auto"
       />
       <div className="text-gray-900 text-lg font-bold">{member.name}</div>
       <div className="text-green-600 font-semibold">{member.role}</div>
@@ -67,25 +100,28 @@ function ProfileCard({ member }) {
         {member.description}
       </div>
       <div className="flex items-center justify-center gap-3 mt-2 w-auto h-5 text-gray-600">
-  <a href="https://twitter.com/username" target="_blank" rel="noopener noreferrer">
-    <i className="fab fa-twitter fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
-  </a>
-  <a href="https://linkedin.com/in/username" target="_blank" rel="noopener noreferrer">
-    <i className="fab fa-linkedin fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
-  </a>
-  <a href="https://github.com/username" target="_blank" rel="noopener noreferrer">
-    <i className="fab fa-github fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
-  </a>
-</div>
-
+        <a href={member.twitterUrl} target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-twitter fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+        </a>
+        <a href={member.linkedinUrl} target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+        </a>
+        <a href={member.githubUrl} target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github fa-lg cursor-pointer transition duration-200 hover:text-green-600"></i>
+        </a>
+      </div>
     </div>
   );
 }
 
 function Nosotros() {
+  const halfLength = Math.ceil(teamMembers.length / 2); 
+
+  const firstHalf = teamMembers.slice(0, halfLength);
+  const secondHalf = teamMembers.slice(halfLength, teamMembers.length);
+
   return (
     <div className="flex flex-col min-h-screen">
-     
       <main className="flex-grow">
         <div className="container mx-auto py-8 bg-cover" 
           style={{
@@ -93,16 +129,18 @@ function Nosotros() {
             "url('https://www.ecovidrio.es/sites/default/files/2019-10/background-home-1.png')",
           }}
         >
-          ...
+          <div className="flex items-center justify-center flex-wrap gap-2 px-12 mb-4">
+            {firstHalf.map((member, index) => (
+              <ProfileCard key={index} member={member} />
+            ))}
+          </div>
           <div className="flex items-center justify-center flex-wrap gap-2 px-12">
-            {teamMembers.map((member) => (
-              <ProfileCard key={member.id} member={member} />  // Renderizamos una tarjeta de perfil para cada miembro del equipo
+            {secondHalf.map((member, index) => (
+              <ProfileCard key={index} member={member} />
             ))}
           </div>
         </div>
       </main>
-
-     
     </div>
   );
 }
