43a1733378a03e4714d5d605d89942e63f4778bd
Marcelo Agustín Gómez Aranda <39355734@alumnos.frsr.utn.edu.ar>
2023-06-17 05:05:25 -0300

    cambios

--- src/components/Home/Pruebita.jsx
+++ src/components/Home/Pruebita.jsx
@@ -12,6 +12,7 @@ function Pruebita() {
       <div
         className="h-auto px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 bg-cover"
         style={{ backgroundImage: `url(${eco})` }}
+  
       >
         <div className=" text-green-700 text-lg h-auto grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
           <div className="mx-auto max-w-lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
