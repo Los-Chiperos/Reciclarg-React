e361a5fde4b366b38cbb839680ab8859c6c1705f
Nicolas <nicolaszelarayan.ez@gmail.com>
2023-06-21 17:56:05 -0300

    modificamos menu Mobile

--- src/components/layout/heder2.jsx
+++ src/components/layout/heder2.jsx
@@ -29,13 +29,11 @@ function Heder2({ isLoggedIn, onLogout, username }) {
 
   return (
     <header>
-      {/* Navbar goes here */}
       <nav className=" gbg-white shadow-lg px-1">
         <div className="max-w-6xl mx-auto px-4">
           <div className="flex justify-between">
             <div className="flex space-x-7">
               <div>
-                {/* Website Logo */}
                 <Link to="/" className="flex items-center ">
                   <img
                     src="http://imgfz.com/i/XecCbuo.png"
@@ -44,35 +42,33 @@ function Heder2({ isLoggedIn, onLogout, username }) {
                   />
                 </Link>
               </div>
-              {/* Primary Navbar items */}
               <div className="hidden md:flex items-center space-x-1">
-  <Link
-    to="/"
-    className={`py-4 px-2 font-semibold ${location.pathname === '/' ? 'text-green-500 border-b-4 border-green-500' : 'text-gray-500 hover:text-green-500 transition duration-300'}`}
-  >
-    Inicio
-  </Link>
-  <Link
-    to="/Nosotros"
-    className={`py-4 px-2 font-semibold ${location.pathname === '/Nosotros' ? 'text-green-500 border-b-4 border-green-500' : 'text-gray-500 hover:text-green-500 transition duration-300'}`}
-  >
-    Nosotros
-  </Link>
-  <Link
-    to="/Encuesta"
-    className={`py-4 px-2 font-semibold ${location.pathname === '/Encuesta' ? 'text-green-500 border-b-4 border-green-500' : 'text-gray-500 hover:text-green-500 transition duration-300'}`}
-  >
-    Encuesta
-  </Link>
-  <Link
-    to="/Faq"
-    className={`py-4 px-2 font-semibold ${location.pathname === '/Faq' ? 'text-green-500 border-b-4 border-green-500' : 'text-gray-500 hover:text-green-500 transition duration-300'}`}
-  >
-    FAQ
-  </Link>
-</div>
+                <Link
+                  to="/"
+                  className={`py-4 px-2 font-semibold ${location.pathname === '/' ? 'text-green-500 border-b-4 border-green-500' : 'text-gray-500 hover:text-green-500 transition duration-300'}`}
+                >
+                  Inicio
+                </Link>
+                <Link
+                  to="/Nosotros"
+                  className={`py-4 px-2 font-semibold ${location.pathname === '/Nosotros' ? 'text-green-500 border-b-4 border-green-500' : 'text-gray-500 hover:text-green-500 transition duration-300'}`}
+                >
+                  Nosotros
+                </Link>
+                <Link
+                  to="/Encuesta"
+                  className={`py-4 px-2 font-semibold ${location.pathname === '/Encuesta' ? 'text-green-500 border-b-4 border-green-500' : 'text-gray-500 hover:text-green-500 transition duration-300'}`}
+                >
+                  Encuesta
+                </Link>
+                <Link
+                  to="/Faq"
+                  className={`py-4 px-2 font-semibold ${location.pathname === '/Faq' ? 'text-green-500 border-b-4 border-green-500' : 'text-gray-500 hover:text-green-500 transition duration-300'}`}
+                >
+                  FAQ
+                </Link>
+              </div>
             </div>
-            {/* Mobile menu button */}
             <div className="md:hidden flex items-center">
               <button
                 className="outline-none mobile-menu-button"
@@ -106,7 +102,6 @@ function Heder2({ isLoggedIn, onLogout, username }) {
                 </svg>
               </button>
             </div>
-            {/* Secondary Navbar items */}
             <div className="hidden md:flex items-center space-x-3">
               {isLoggedIn ? (
                 <>
@@ -140,52 +135,78 @@ function Heder2({ isLoggedIn, onLogout, username }) {
               )}
             </div>
           </div>
-          {/* Mobile Menu open: "block", Menu closed: "hidden" */}
           <div className={`mobile-menu ${showMenu ? "block" : "hidden"}`}>
             <ul className="pt-4 pb-3">
-              {!isLoggedIn ? (
+              {isLoggedIn ? (
                 <>
                   <li>
-                    <Link
-                      to="/registrar"
-                      className="text-center block pl-4 text-sm font-semibold text-gray-900 hover:bg-green-500 hover:text-white transition duration-300 py-2"
+                    <Link to={`/userProfile/${username}`}
+                      className="block pl-4 text-sm font-semibold text-gray-900 hover:bg-green-500 hover:text-white transition duration-300 py-2"
                     >
-                      Registrarse
+                      Bienvenido, {username}
                     </Link>
                   </li>
                   <li>
                     <Link
+                      to="/"
+                      onClick={handleLogout}
+                      className="block pl-4 text-sm font-semibold text-gray-900 hover:bg-green-500 hover:text-white transition duration-300 py-2"
+                    >
+                      Cerrar sesión
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <Link
                       to="/login"
-                      className="text-center block pl-4 text-sm font-semibold text-gray-900 hover:bg-green-500 hover:text-white transition duration-300 py-2"
+                      className="block pl-4 text-sm font-semibold text-gray-900 hover:bg-green-500 hover:text-white transition duration-300 py-2"
                     >
                       Loguearse
                     </Link>
                   </li>
+                  <li>
+                    <Link
+                      to="/registrar"
+                      className="block pl-4 text-sm font-semibold text-gray-900 hover:bg-green-500 hover:text-white transition duration-300 py-2"
+                    >
+                      Registrarse
+                    </Link>
+                  </li>
                 </>
-              ) : null}
+              )}
               <li>
                 <Link
                   to="/"
-                  className="block pl-4 text-sm font-semibold text-gray-900 hover:bg-green-500 transition hover:text-white duration-300 py-2"
+                  className={`block pl-4 text-sm font-semibold ${location.pathname === '/' ? 'text-green-500' : 'text-gray-900 hover:bg-green-500 hover:text-white transition duration-300'} py-2`}
                 >
                   Inicio
                 </Link>
               </li>
               <li>
-                <a
-                  href="/recompensa.html"
-                  className="block pl-4 text-sm font-semibold text-gray-900 hover:bg-green-500 transition hover:text-white duration-300 py-2"
+                <Link
+                  to="/Nosotros"
+                  className={`block pl-4 text-sm font-semibold ${location.pathname === '/Nosotros' ? 'text-green-500' : 'text-gray-900 hover:bg-green-500 hover:text-white transition duration-300'} py-2`}
                 >
-                  recompensas
-                </a>
+                  Nosotros
+                </Link>
               </li>
               <li>
-                <a
-                  href=""
-                  className="block pl-4 text-sm font-semibold text-gray-900 hover:bg-green-500 transition hover:text-white duration-300 py-2"
+                <Link
+                  to="/Encuesta"
+                  className={`block pl-4 text-sm font-semibold ${location.pathname === '/Encuesta' ? 'text-green-500' : 'text-gray-900 hover:bg-green-500 hover:text-white transition duration-300'} py-2`}
                 >
-                  Encuestas
-                </a>
+                  Encuesta
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/Faq"
+                  className={`block pl-4 text-sm font-semibold ${location.pathname === '/Faq' ? 'text-green-500' : 'text-gray-900 hover:bg-green-500 hover:text-white transition duration-300'} py-2`}
+                >
+                  FAQ
+                </Link>
               </li>
             </ul>
           </div>
@@ -196,4 +217,3 @@ function Heder2({ isLoggedIn, onLogout, username }) {
 }
 
 export default Heder2;
-
